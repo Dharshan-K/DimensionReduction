876377d9c35312e5395f7562610a152f99f6dda3
dharshan <kdharshanprog@gmail.com>
2024-04-24 23:35:21 +0530

    PCA complete.

--- src/converter/principal_component_analysis.rs
+++ src/converter/principal_component_analysis.rs
@@ -1,6 +1,7 @@
-use std::vec::{self, Vec};
-use ndarray::{Array2, Axis, Zip}; 
+use std::vec::{self};
+use ndarray::{Array2, Axis}; 
 use ndarray::prelude::{ArrayBase,Dim};
+use nalgebra::{SymmetricEigen,DMatrix};
 
 pub struct PCA{ 
     input_matrix: Array2<f64>,
@@ -12,22 +13,20 @@ impl PCA{
     }
     pub fn calculate_mean(&mut self) {        
         let mut matrix_mean: ArrayBase<ndarray::OwnedRepr<f64>, Dim<[usize; 1]>> = self.input_matrix.mean_axis(Axis(0)).unwrap();
+        let centered_matrix = &self.input_matrix - &matrix_mean;
+        // print!("{:?}", centered_matrix);
+        let input_size = self.input_matrix.shape()[0] as f64;
+        let covariance_matrix = &centered_matrix.t().dot(&centered_matrix) / (&input_size - 1.0);
+        let covariance_matrix_nalgebra = DMatrix::from_iterator(covariance_matrix.nrows(), covariance_matrix.ncols(), covariance_matrix.iter().copied());
+        let eigen_matrix = SymmetricEigen::new(covariance_matrix_nalgebra);
+        let eigen_values = eigen_matrix.eigenvalues;
+        let eigen_vectors = eigen_matrix.eigenvectors;
 
-        // let mut covariance_matrix = Zip::from(&self.input_matrix).and(&matrix_mean).map_collect(|&x,&y| x - y);
+        println!("Eigenvalues: {:?}", eigen_values);
+        println!("Eigenvectors: {:?}", eigen_vectors);
+
+        // print!("{:?}", covariance_matrix);
         
-        for i in 0..self.input_matrix.len_of(Axis(0)){
-            print!("{}", matrix_mean[i]);
-        }  
-
-        
-
-        // let mut covarince_matrix = self.input_matrix - matrix_mean;     
-
-        // if(length==1){
-        //     return Some(vec![matrix_mean])
-        // }else {
-        //     return Some(vec![matrix_mean])
-        // }
     }     
 
        
